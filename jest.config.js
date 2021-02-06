89a633dc63038624033cd3b7a77a32aca0b05078
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-02-06 21:37:36 +0100

    test: generate @internal/test-util

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/packages/spectacular',
     '<rootDir>/tools',
     '<rootDir>/packages/examples/tour-of-heroes/crisis-center',
+    '<rootDir>/packages/internal/test-util',
   ],
 };
---
+++ packages/internal/test-util/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/internal-test-util.module';
