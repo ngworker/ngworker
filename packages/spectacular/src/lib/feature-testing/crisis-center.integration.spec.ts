71cff13b97802663164497ab064bd5255cf8d49c
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-02-06 22:00:49 +0100

    test: scaffold crisis center integration test suite

---
+++ packages/spectacular/src/lib/feature-testing/crisis-center.integration.spec.ts
@@ -0,0 +1,20 @@
+import { ComponentFixture } from '@angular/core/testing';
+import { CrisisCenterModule, crisisCenterPath } from '@tour-of-heroes/crisis-center';
+
+import { SpectacularAppComponent } from '../application-testing/app-component/spectacular-app.component';
+import { SpectacularFeatureTestbed } from './testbed/spectacular-feature-testbed';
+
+describe('Tour of Heroes: Crisis center integration tests', () => {
+  beforeEach(() => {
+    rootFixture = SpectacularFeatureTestbed.createFeature({
+      featureModule: CrisisCenterModule,
+      featurePath: crisisCenterPath,
+    });
+  });
+
+  let rootFixture: ComponentFixture<SpectacularAppComponent>;
+
+  it('works', () => {
+    expect(rootFixture).toBeDefined();
+  });
+});
