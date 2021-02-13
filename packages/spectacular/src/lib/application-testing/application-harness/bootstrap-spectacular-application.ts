6ce8d5c55177c32c50e7309fe9a7d0cb0097e651
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-02-13 23:01:45 +0100

    feat: introduce SpectacularApplicationHarness

--- packages/spectacular/src/lib/application-testing/application-harness/bootstrap-spectacular-application.ts
+++ packages/spectacular/src/lib/application-testing/application-harness/bootstrap-spectacular-application.ts
@@ -6,6 +6,7 @@ import { SpectacularAppComponent, spectacularAppTag } from '../app-component/spe
 import { SpectacularAppModule } from '../app-component/spectacular-app.module';
 import { bootstrapComponent } from '../util-bootstrapping/bootstrap-component';
 import { BootstrapSpectacularApplicationOptions } from './bootstrap-spectacular-application-options';
+import { SpectacularApplicationHarness } from './spectacular-application-harness';
 
 /**
  * Bootstrap a test application with the specified metadata. Useful to test
@@ -18,7 +19,7 @@ import { BootstrapSpectacularApplicationOptions } from './bootstrap-spectacular-
 export function bootstrapSpectacularApplication({
   imports = [],
   providers = [],
-}: BootstrapSpectacularApplicationOptions): void {
+}: BootstrapSpectacularApplicationOptions): SpectacularApplicationHarness {
   TestBed.configureTestingModule({
     imports: [RouterTestingModule, ...imports, SpectacularAppModule],
     providers: [...providers],
@@ -42,4 +43,12 @@ export function bootstrapSpectacularApplication({
     ngZone: TestBed.inject(NgZone),
     tag: spectacularAppTag,
   });
+
+  return {
+    inject: TestBed.inject.bind(TestBed),
+    get rootComponent() {
+      return rootFixture.componentInstance;
+    },
+    rootFixture,
+  };
 }
---
+++ packages/spectacular/src/lib/application-testing/application-harness/spectacular-application-harness.ts
@@ -0,0 +1,39 @@
+import { AbstractType, InjectFlags, InjectionToken, Type } from '@angular/core';
+import { ComponentFixture } from '@angular/core/testing';
+
+import { SpectacularAppComponent } from '../../application-testing/app-component/spectacular-app.component';
+
+/**
+ * A harness for testing application-level software artifacts.
+ */
+export interface SpectacularApplicationHarness {
+  /**
+   * Resolve a dependency based on the specified dependency injection token.
+   *
+   * @param token The token representing the dependency, that is a class or an
+   *   `InjectionToken`.
+   * @param notFoundValue The default value in case the specified dependency
+   *   has not been provided. Optional. Default is `null`.
+   * @param flags Dependency injection options, for example
+   *   `InjectFlags.Optional | InjectFlags.SkipSelf`. Optional. Default is
+   *   `InjectFlags.Default`.
+   */
+  inject<T>(
+    token: Type<T> | InjectionToken<T> | AbstractType<T>,
+    notFoundValue?: T,
+    flags?: InjectFlags
+  ): T;
+  inject<T>(
+    token: Type<T> | InjectionToken<T> | AbstractType<T>,
+    notFoundValue: null,
+    flags?: InjectFlags
+  ): T | null;
+  /**
+   * The bootstrapped component.
+   */
+  readonly rootComponent: SpectacularAppComponent;
+  /**
+   * The component fixture for the bootstrapped component.
+   */
+  readonly rootFixture: ComponentFixture<SpectacularAppComponent>;
+}
