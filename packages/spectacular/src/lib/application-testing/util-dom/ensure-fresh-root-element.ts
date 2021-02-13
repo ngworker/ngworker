ca3171d477f31296026acd845a0d38e4ef280c71
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-02-13 21:53:21 +0100

    refactor: rename ensureFreshRootElement

--- packages/spectacular/src/lib/application-testing/util-dom/ensure-fresh-root-element.ts
+++ packages/spectacular/src/lib/application-testing/util-dom/ensure-fresh-root-element.ts
@@ -1,7 +1,13 @@
-import { createRootElement } from './create-root-element';
+import { createElement } from './create-element';
 import { replaceRootElement } from './replace-root-element';
 
-export function ensureRootElement(tagName: string): void {
-  const rootElement = createRootElement(tagName);
+/**
+ * Make sure that we have a new root element attached to the DOM.
+ *
+ * @param tagName The tag name of the element to create and attach.
+ */
+export function ensureFreshRootElement(tagName: string): void {
+  const rootElement = createElement(tagName);
+
   replaceRootElement(rootElement);
 }
