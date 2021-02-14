33be9661c5a6830c8342f484d9eceaf4f8e9beee
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-02-14 02:02:40 +0100

    test: cover declarables

--- packages/spectacular/src/lib/pipe-testing/pipe-harness/create-pipe-harness.spec.ts
+++ packages/spectacular/src/lib/pipe-testing/pipe-harness/create-pipe-harness.spec.ts
@@ -1,4 +1,4 @@
-import { Injectable, NgModule } from '@angular/core';
+import { Injectable, NgModule, Pipe, PipeTransform } from '@angular/core';
 
 import { PassthroughPipe, passthroughPipeName } from '../test-util/passthrough.pipe';
 import { createPipeHarness } from './create-pipe-harness';
@@ -6,35 +6,56 @@ import { createPipeHarness } from './create-pipe-harness';
 describe(createPipeHarness.name, () => {
   describe('Configuration', () => {
     @Injectable()
-    class JobService {}
+    class PipeService {}
 
     @NgModule({
-      providers: [JobService],
+      providers: [PipeService],
     })
-    class JobServiceModule {}
+    class PipeServiceModule {}
 
     it('adds the specified imports', () => {
       const harness = createPipeHarness({
-        imports: [JobServiceModule],
+        imports: [PipeServiceModule],
         pipeType: PassthroughPipe,
         pipeName: passthroughPipeName,
         value: null,
       });
 
-      const jobService = harness.inject(JobService);
-      expect(jobService).toBeInstanceOf(JobService);
+      const jobService = harness.inject(PipeService);
+      expect(jobService).toBeInstanceOf(PipeService);
     });
 
     it('adds the specified providers', () => {
       const harness = createPipeHarness({
-        providers: [JobService],
+        providers: [PipeService],
         pipeType: PassthroughPipe,
         pipeName: passthroughPipeName,
         value: null,
       });
 
-      const jobService = harness.inject(JobService);
-      expect(jobService).toBeInstanceOf(JobService);
+      const jobService = harness.inject(PipeService);
+      expect(jobService).toBeInstanceOf(PipeService);
+    });
+
+    it('adds the specified declarables', () => {
+      @Pipe({
+        name: 'testRepeat',
+      })
+      class RepeatPipe implements PipeTransform {
+        transform(value: string, times: number) {
+          return value.repeat(times);
+        }
+      }
+
+      const harness = createPipeHarness({
+        declarations: [RepeatPipe],
+        pipeType: PassthroughPipe,
+        pipeName: passthroughPipeName,
+        template: `{{ value | ${passthroughPipeName} | testRepeat:4 }}`,
+        value: 'Boom',
+      });
+
+      expect(harness.text).toBe('BoomBoomBoomBoom');
     });
   });
 });
