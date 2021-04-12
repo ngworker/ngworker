0bb09230d2b665c81e9f397551b0dd3d6d0463bc
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-04-12 18:40:11 +0200

    feat: generate schematics plugin package

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,7 @@ module.exports = {
     '<rootDir>/tools',
     '<rootDir>/packages/examples/tour-of-heroes/crisis-center',
     '<rootDir>/packages/internal/test-util',
+    '<rootDir>/packages/schematics',
+    '<rootDir>/e2e\\schematics-e2e',
   ],
 };
---
+++ packages/schematics/src/generators/schematics/schema.d.ts
@@ -0,0 +1,5 @@
+export interface SchematicsGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
