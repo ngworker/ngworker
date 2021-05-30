aef2c712b91fc6d9e6cb5ae60eb1bba52e1adfa6
Serkan Sipahi <serkan.sipahi@yahoo.de>
2021-05-30 15:17:17 +0200

    feat: cleanup

--- e2e/mat-table-spreadsheet/src/app/app.module.ts
+++ e2e/mat-table-spreadsheet/src/app/app.module.ts
@@ -7,10 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,12 +18,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule,
 
     CdkSpreadsheetModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
