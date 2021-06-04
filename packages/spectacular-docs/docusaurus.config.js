ce97bd56bb7055687f16c953ada1f77c4775f603
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-05 01:28:41 +0200

    docs: change base URL

--- packages/spectacular-docs/docusaurus.config.js
+++ packages/spectacular-docs/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://ngworker.github.io',
-  baseUrl: '/ngworker/',
+  baseUrl: '/ngworker/spectacular',
   onBrokenLinks: 'error',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
