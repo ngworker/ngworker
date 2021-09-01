34050c5e8c698a46a8ae8d584880b220300a5184
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-01 22:15:04 +0200

    ci: deploy Spectacular docs to GitHub Pages (#25)
    - Add Docusaurus docs project for Spectacular
    - Deploy Spectacular documentation to GitHub Pages: https://ngworker.github.io/ngworker/
    
    Included in documentation:
    - API reference
    - Application testing API introduction
    - Feature testing API introduction
    - Pipe testing API introduction
    - Spectacular introduction
    - Getting started with Spectacular

---
+++ packages/spectacular-docs/docusaurus.config.js
@@ -0,0 +1,136 @@
+const shadesOfPurpleTheme = require('prism-react-renderer/themes/shadesOfPurple');
+
+const organizationName = 'ngworker';
+const projectName = 'ngworker';
+
+module.exports = {
+  baseUrl: '/ngworker/',
+  favicon: 'img/favicon.ico',
+  onBrokenLinks: 'error',
+  onBrokenMarkdownLinks: 'warn',
+  organizationName,
+  plugins: [
+    [
+      'docusaurus-plugin-typedoc',
+      {
+        entryPoints: ['./packages/spectacular/src/index.ts'],
+        excludeInternal: true,
+        excludePrivate: true,
+        tsconfig: './packages/spectacular/tsconfig.lib.prod.json',
+      },
+    ],
+  ],
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/ngworker/ngworker/edit/main/packages/spectacular-docs',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/ngworker/ngworker/edit/main/packages/spectacular-docs/blog',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  projectName,
+  tagline: 'Spectacular Angular integration testing.',
+  title: 'Spectacular',
+  themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    image: 'img/logo.png',
+    footer: {
+      copyright: `© ${new Date().getFullYear()} ngworkers. Logo by Felipe Zambrano. Artwork by vectorjuice.`,
+      links: [
+        {
+          title: 'Documentation',
+          items: [
+            {
+              label: 'Spectacular',
+              to: 'docs/',
+            },
+            {
+              label: 'Application testing',
+              to: 'docs/application-testing/',
+            },
+            {
+              label: 'Feature testing',
+              to: 'docs/feature-testing/',
+            },
+            {
+              label: 'Pipe testing',
+              to: 'docs/pipe-testing/',
+            },
+            {
+              label: 'API reference',
+              to: 'docs/api/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/spectacular',
+            },
+            {
+              label: 'YouTube',
+              href:
+                'https://www.youtube.com/results?search_query=spectacular+ngworker',
+            },
+          ],
+        },
+        {
+          title: 'Package',
+          items: [
+            {
+              href: `https://github.com/${organizationName}/${projectName}`,
+              label: 'GitHub',
+            },
+            {
+              href: 'https://npmjs.com/package/@ngworker/spectacular',
+              label: 'NPM',
+            },
+          ],
+        },
+      ],
+      logo: {
+        alt: 'Spectacular logo',
+        href: '/',
+        src: 'img/logo.png',
+      },
+      style: 'dark',
+    },
+    navbar: {
+      items: [
+        {
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+          to: 'docs/',
+        },
+      ],
+      logo: {
+        alt: 'Spectacular logo',
+        src: 'img/logo.png',
+      },
+      title: 'Spectacular',
+    },
+    prism: {
+      darkTheme: shadesOfPurpleTheme,
+      defaultLanguage: 'typescript',
+      theme: shadesOfPurpleTheme,
+    },
+  },
+  url: 'https://ngworker.github.io',
+};
---
+++ packages/spectacular-docs/src/pages/index.js
@@ -0,0 +1,105 @@
+import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import React from 'react';
+
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: 'Flexible when you need it',
+    imageUrl: 'img/vectorjuice-flexible.jpg',
+    description: (
+      <>
+        Spectacular's test harnesses run your Angular feature in an environment
+        as reasonably close to a real Angular application as possible. You can
+        easily add custom Angular module imports and providers to replace
+        dependencies when needed.
+      </>
+    ),
+  },
+  {
+    title: 'Specialized test harnesses',
+    imageUrl: 'img/vectorjuice-specialized.png',
+    description: (
+      <>
+        Spectacular reduces boilerplate by taking care of setup and
+        configuration of the Angular testing module. Every test harness focuses
+        on testing a specific part of Angular applications and libraries.
+      </>
+    ),
+  },
+  {
+    title: 'Faster than end-to-end tests',
+    imageUrl: 'img/vectorjuice-fast.jpg',
+    description: (
+      <>
+        Test parts of your Angular project without spinning up an entire
+        application. Spectacular uses Angular's testbed under the hood which is
+        continuously getting faster as new versions are released. Compiled
+        classes are cached by Angular between test cases.
+      </>
+    ),
+  },
+];
+
+function Feature({ imageUrl, title, description }) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout
+      title={'Spectacular by ngworkers'}
+      description="Spectacular Angular integration testing. Flexible when you need it. Specialized test harnesses. Faster than end-to-end tests."
+    >
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted
+              )}
+              to={useBaseUrl('docs/')}
+            >
+              Get started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
