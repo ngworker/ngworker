2184e70b4f661c43bdf3821783800ae68a569f08
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-07-23 20:11:23 +0200

    feat!: migrate to Nx 12.10, Angular 12.x (#42)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
   projects: [
+    ...getJestProjects(),
     '<rootDir>/packages/spectacular',
     '<rootDir>/tools',
     '<rootDir>/packages/examples/tour-of-heroes/crisis-center',
