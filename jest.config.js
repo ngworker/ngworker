4d90cd5d7022522334ff06b37473984b5034f9f8
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-07-23 21:12:15 +0200

    test: remove duplicate Jest projects (#44)

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 const { getJestProjects } = require('@nrwl/jest');
 
 module.exports = {
-  projects: [
-    ...getJestProjects(),
-    '<rootDir>/packages/spectacular',
-    '<rootDir>/tools',
-    '<rootDir>/packages/examples/tour-of-heroes/crisis-center',
-    '<rootDir>/packages/internal/test-util',
-  ],
+  projects: [...getJestProjects()],
 };
