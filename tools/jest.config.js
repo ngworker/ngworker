779b1893f280d112938ea5436561fb1a18026118
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-07-07 23:48:17 +0200

    chore: align workspace with Nx 13.1 defaults

--- tools/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  displayName: 'workspace-tools',
-  preset: '../jest.preset.js',
-  globals: {
-    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
-  },
-  coverageDirectory: '../coverage/tools',
-};
