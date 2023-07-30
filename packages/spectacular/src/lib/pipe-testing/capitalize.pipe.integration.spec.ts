440b9a92301a44ae6ee5c85eaa6d714fc822133a
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-07-31 01:10:35 +0200

    test(spectacular): add test of standalone `CapitalizePipe`

--- packages/spectacular/src/lib/pipe-testing/capitalize.pipe.integration.spec.ts
+++ packages/spectacular/src/lib/pipe-testing/capitalize.pipe.integration.spec.ts
@@ -1,35 +1,57 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { createPipeHarness } from './pipe-harness/create-pipe-harness';
 
+function createCapitalizePipe({
+  standalone = true,
+}: {
+  readonly name: string;
+  readonly standalone?: boolean;
+}) {
+  @Pipe({
+    standalone,
+    name: capitalizePipeName,
+  })
+  class CapitalizePipe implements PipeTransform {
+    transform(value: string): string {
+      return value
+        .split(/\s+/g)
+        .map(word => word[0].toUpperCase() + word.substring(1))
+        .join(' ');
+    }
+  }
+
+  return CapitalizePipe;
+}
+
 const capitalizePipeName = 'capitalize';
 
-function setup({ value }: { readonly value: string }) {
-  const harness = createPipeHarness({
-    pipe: CapitalizePipe,
-    pipeName: capitalizePipeName,
-    value,
+describe('CapitalizePipe', () => {
+  it('[Classic] capitalizes every word of the text', () => {
+    const harness = createPipeHarness({
+      pipe: createCapitalizePipe({
+        standalone: false,
+        name: capitalizePipeName,
+      }),
+      pipeName: capitalizePipeName,
+      value: 'mr. potato head',
+    });
+
+    expect(harness.text).toBe('Mr. Potato Head');
+
+    harness.value = 'ms. potato head';
+
+    expect(harness.text).toBe('Ms. Potato Head');
   });
 
-  return {
-    harness,
-  };
-}
-
-@Pipe({
-  name: capitalizePipeName,
-})
-export class CapitalizePipe implements PipeTransform {
-  transform(value: string): string {
-    return value
-      .split(/\s+/g)
-      .map(word => word[0].toUpperCase() + word.substring(1))
-      .join(' ');
-  }
-}
-
-describe(CapitalizePipe.name, () => {
-  it('capitalizes every word of the text', () => {
-    const { harness } = setup({ value: 'mr. potato head' });
+  it('[Standalone] capitalizes every word of the text', () => {
+    const harness = createPipeHarness({
+      pipe: createCapitalizePipe({
+        standalone: true,
+        name: capitalizePipeName,
+      }),
+      pipeName: capitalizePipeName,
+      value: 'mr. potato head',
+    });
 
     expect(harness.text).toBe('Mr. Potato Head');
 
