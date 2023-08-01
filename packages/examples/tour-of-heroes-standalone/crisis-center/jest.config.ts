ad4c2a22167fd9cfd8bb3e6b37cde80d3176f5d6
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-08-01 16:11:37 +0200

    test: generate `@tour-of-heroes-standalone/crisis-center` library

---
+++ packages/examples/tour-of-heroes-standalone/crisis-center/jest.config.ts
@@ -0,0 +1,23 @@
+/* eslint-disable */
+export default {
+  displayName: 'examples-tour-of-heroes-standalone-crisis-center',
+  preset: '../../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory:
+    '../../../../coverage/packages/examples/tour-of-heroes-standalone/crisis-center',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
